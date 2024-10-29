87e4a61b30bed713d404e86791117d733e04734b
ropats16 <rohitcpathare@gmail.com>
2024-10-29 18:00:55 +0530

    Update game context

--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -1,21 +1,32 @@
-"use client"
+"use client";
 
 import { createContext, useContext, useState, ReactNode } from "react";
 
 type GameState = "landing" | "waiting" | "inGame";
 
-interface Player {
-  id: number;
+export interface Player {
+  id: string;
+  bazarId?: string;
   name: string;
+  image?: string;
+  score?: number;
+  isCreator?: boolean;
 }
 
 interface GameContextType {
   state: GameState;
   setState: (newState: GameState) => void;
   currentPlayer: Player | null;
-  setCurrentPlayer: (player: Player) => void;
+  setCurrentPlayer: (player: Player | null) => void;
   joinedUsers: Player[];
   setJoinedUsers: (players: Player[]) => void;
+  gameProcess: string;
+  activeDrawer: string;
+  setActiveDrawer: (drawer: string) => void;
+  currentRound: number;
+  setCurrentRound: (round: number) => void;
+  maxRounds: number;
+  // setGameProcess: (process: string) => void;
 }
 
 const GameContext = createContext<GameContextType | undefined>(undefined);
@@ -24,9 +35,30 @@ export const GameProvider = ({ children }: { children: ReactNode }) => {
   const [state, setState] = useState<GameState>("landing");
   const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
   const [joinedUsers, setJoinedUsers] = useState<Player[]>([]);
+  // const [gameProcess, setGameProcess] = useState<string>("");
+  const gameProcess = "DVfmQJsqhoRxPdr5U9fUVCSz4n0qyhyw_Dtlj2_QEuc";
+  const [activeDrawer, setActiveDrawer] = useState<string>("");
+  const [currentRound, setCurrentRound] = useState<number>(1);
+  const maxRounds = 8;
 
   return (
-    <GameContext.Provider value={{ state, setState, currentPlayer, setCurrentPlayer, joinedUsers, setJoinedUsers }}>
+    <GameContext.Provider
+      value={{
+        state,
+        setState,
+        currentPlayer,
+        setCurrentPlayer,
+        joinedUsers,
+        setJoinedUsers,
+        gameProcess,
+        // setGameProcess,
+        activeDrawer,
+        setActiveDrawer,
+        currentRound,
+        setCurrentRound,
+        maxRounds,
+      }}
+    >
       {children}
     </GameContext.Provider>
   );
