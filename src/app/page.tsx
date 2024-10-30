fde1605f44a917b07dbb7f8d4ab89d53b973f63a
ropats16 <rohitcpathare@gmail.com>
2024-10-30 15:11:07 +0530

    Add dynamic lazy loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 "use client";
 
-import LandingPage from "@/components/LandingPage";
-import WaitingRoom from "@/components/WaitingRoom";
-import GameRound from "@/components/GameRound";
+// import LandingPage from "@/components/LandingPage";
+// import WaitingRoom from "@/components/WaitingRoom";
+// import GameRound from "@/components/GameRound";
 import { useGameContext } from "@/context/GameContext";
 import { useConnection } from "arweave-wallet-kit";
+
+import dynamic from "next/dynamic";
+
+const LandingPage = dynamic(() => import("@/components/LandingPage"), {
+  ssr: false,
+});
+
+const WaitingRoom = dynamic(() => import("@/components/WaitingRoom"), {
+  ssr: false,
+});
+
+const GameRound = dynamic(() => import("@/components/GameRound"), {
+  ssr: false,
+});
 
 export default function SketchGuessApp() {
   const { mode, setMode, setCurrentPlayer } = useGameContext();
