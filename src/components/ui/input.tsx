2ee62b8a9a6a89d4c7d4de41c5b635f8a53d1308
ropats16 <rohitcpathare@gmail.com>
2024-10-30 14:14:45 +0530

    Lint config cleanup

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,9 +1,13 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+// /* eslint-disable @typescript-eslint/no-empty-interface */
+// export interface InputProps
+//   extends React.InputHTMLAttributes<HTMLInputElement> {}
+// /* eslint-enable @typescript-eslint/no-empty-interface */
+
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -17,9 +21,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
